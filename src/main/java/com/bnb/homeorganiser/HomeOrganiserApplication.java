70cf0ecf8e52956a3f7dc75d0e6081c3b996258d
Ulula Bubo <ulula.bubo@gmail.com>
2017-11-23 17:12:35 +0100

    Initial commit

---
+++ src/main/java/com/bnb/homeorganiser/HomeOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.bnb.homeorganiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeOrganiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnb/homeorganiser/IndexControllerTest.java
@@ -0,0 +1,39 @@
+package com.bnb.homeorganiser;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.bnb.homeorganiser.HomeOrganiserApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { HomeOrganiserApplication.class })
+@WebAppConfiguration
+public class IndexControllerTest {
+
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+
+	@Test
+	public void showIndexPage() throws Exception {
+		mockMvc.perform(get("/index")).andExpect(status().isOk());
+	}
+
+}
